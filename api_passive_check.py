693a4388a4f63fc05089005c746c93be1eac11aa
AtlasWiki <nathanwong3421@gmail.com>
2024-06-11 22:49:59 -0700

    add working prototype for api check

--- api_passive_check.py
+++ api_passive_check.py
@@ -2,10 +2,19 @@ from meowpi_package.utils import parse_domain, parse_dirs
 from meowpi_package.shared import all_dirs, dict_report, to_remove, to_add
 
 def api_check():
-    for dir in all_dirs[:]:
-        *formatted_dir, unformatted_dir = parse_dirs(dir)
-    for dir in all_dirs[:]:
-        for domain in parse_domain(dir):
-            if "api" in domain.lower():
-                print(domain)
-    
+    api_scoreboard = {}
+    for url in all_dirs[:]:
+        *formatted_dirs, unformatted_dirs = parse_dirs(url)
+        for dir in unformatted_dirs: 
+            if not "auth" in dir:
+                pass
+            elif "auth" in dir:
+                print(f"[API] [KEYWORD HIT] {url}")
+                api_scoreboard.update({f"{url}":1})
+            else:
+                for domain in parse_domain(url):
+                    if "api" in domain.lower():
+                        print(f"[API] [KEYWORD HIT] {url}")
+                        api_scoreboard.update({f"{url}":1})
+
+    print(api_scoreboard)
