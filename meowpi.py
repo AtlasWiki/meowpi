3fb76c8bb8bc5e1c54185312c4c55081cac03aa1
AtlasWiki <nathanwong3421@gmail.com>
2024-06-18 21:53:47 -0700

    add support to read stdin urls

--- meowpi.py
+++ meowpi.py
@@ -3,7 +3,7 @@ from meowpi_package.args import argparser
 from meowpi_package.api_active_check import api_act_check
 from meowpi_package.shared import all_dirs
 from meowpi_package.api_passive_check import api_pass_check 
-import asyncio
+import asyncio, sys
 
 intro_logo = f"""\033[35m
         ▄───▄      
@@ -22,9 +22,14 @@ intro_logo = f"""\033[35m
 
 if __name__ == "__main__":
     args = argparser()
-    with open(f'{args.file}','r') as urls:
-        for line in urls:
-            all_dirs.append(line.strip('\n'))
+    try:
+        with open(f'{args.file}','r') as urls:
+            for line in urls:
+                all_dirs.append(line.strip('\n'))
+    except Exception:
+        for url in sys.stdin:
+            all_dirs.append(url.strip("\n"))
+
     if not (args.stdout):
         print(intro_logo)
     if not (args.passive):
--- meowpi_package/args.py
+++ meowpi_package/args.py
@@ -26,7 +26,7 @@ def argparser():
     \u001b[91mstdout:\u001b[0m python {get_py_filename } urls.txt -S   
     ''', formatter_class=NewlineFormatter, usage=f'{intro_logo}\n\u001b[32m%(prog)s [options] file_name\u001b[0m')
 
-    parser.add_argument("file", help="\u001b[96mspecify file/list of urls")
+    parser.add_argument("-f, --file", help="\u001b[96mspecify file/list of urls")
     parser.add_argument("-s", "--stdout", help="stdout friendly, displays urls only in stdout compatibility. also known as silent mode", action="store_true")
     parser.add_argument("-m", "--mode", help="filter modes", choices=['all', '1xx', '2xx', '3xx', '4xx', '5xx', 'forbidden'])
     parser.add_argument("-p", "--passive", help="enables passive API filtering only and disables active API probing", action="store_true")
